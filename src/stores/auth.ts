1e96432fa320afc9c89634e9d5494bb4c9514372
Tran Nhan <nhandev10@gmail.com>
2025-08-16 09:34:43 +0700

    fix: persist auth and add routes

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,16 +1,26 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
 type AuthState = {
-    accessToken: string | null;
-    user: {id: number; email: string; roles: string[] } | null;
-    setAccessToken: (t: string | null) => void;
-    setUser: (u : AuthState['user']) => void;
-    reset: () => void;
+  accessToken: string | null;
+  user: { id: number; email: string; roles: string[] } | null;
+  setAccessToken: (t: string | null) => void;
+  setUser: (u: AuthState["user"]) => void;
+  reset: () => void;
 };
 
-export const useAuthStore = create<AuthState>((set) => ({
-    accessToken: null,
-    user: null,
-    setAccessToken: (t) => set({accessToken: t}),
-    setUser: (u) => set({user: u}),
-    reset: () => set({accessToken: null, user: null}),
-}));
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      accessToken: null,
+      user: null,
+      setAccessToken: (t) => set({ accessToken: t }),
+      setUser: (u) => set({ user: u }),
+      reset: () => set({ accessToken: null, user: null }),
+    }),
+    {
+      name: "auth", // key in localStorage
+    },
+  ),
+);
+
